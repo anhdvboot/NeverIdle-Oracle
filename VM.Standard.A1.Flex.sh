77405bd2a6da7d0ee199f949e30a713cdf6062f4
anhdv <167644050+anhdvboot@users.noreply.github.com>
2026-06-30 13:44:13 +0700

    Update VM.Standard.A1.Flex.sh

--- VM.Standard.A1.Flex.sh
+++ VM.Standard.A1.Flex.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 bash <(curl -s -L https://raw.githubusercontent.com/anhdvboot/NeverIdle-Oracle/refs/heads/main/onekey-NeverIdle.sh)
 pkill NeverIdle
-/tmp/NeverIdle -c 12h -cp 0.2 -m 5 -n 12h
+/tmp/NeverIdle -cp 0.3 -m 5
 
 # Nếu VPS chưa chạy gì thì nên để là -cp và giảm số lần speed test xuống, hiện speedtest lỗ, bỏ luôn -n 2h
-# /tmp/NeverIdle -cp 0.3 -m 5 -n 2h
+# /tmp/NeverIdle -cp 0.3 -m 5
+# /tmp/NeverIdle -c 12h -cp 0.2 -m 5 -n 12h
